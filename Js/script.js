8c2d2eb509eafa12d591df69ad077b7b7cdd0baf
Steve99-coder <stevenderitu99@gmail.com>
2020-10-03 23:19:13 +0300

    Add javascript get method date

--- Js/script.js
+++ Js/script.js
@@ -4,3 +4,9 @@ function check() {
     var month = document.getElementById("month").value;
     var femaleNames=[ "Akosua", "Adwoa", "Abenaa","Akua", "Yaa", "Afua", "Ama"];
     var maleNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+    var cap=new Date(date,month,year);
+    cap.setDate(parseInt(date));
+    cap.setMonth(parseInt(month)-1);
+    cap.setFullYear(parseInt(year));
+    var cad = cap.getDay();
