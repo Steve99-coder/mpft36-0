494d1f0c4a953fad5717b523fabc53bb5555d0df
Steve99-coder <stevenderitu99@gmail.com>
2020-10-03 23:09:21 +0300

    Add function name and declaration of the variables date,year,month,femaleNames,maleNames

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+function check() {
+    var date = document.getElementById("date").value;
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var femaleNames=[ "Akosua", "Adwoa", "Abenaa","Akua", "Yaa", "Afua", "Ama"];
+    var maleNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
